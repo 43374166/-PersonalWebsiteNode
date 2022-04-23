4a91e99d113c0fef88d3c94cc63aa5ef12e8b9c3
Lico <74867220+43374166@users.noreply.github.com>
2022-04-23 14:21:41 +0800

    Delete server directory

--- server/src/utils/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  jwtSecretKey: 'FishDream 520 @Lico', // 加密和解密的token密钥
-  expiresIn: '2400h', // token生效时间
-  algorithms: ['HS256'] // 设置算法express-jwt
-}
--- server/src/utils/redis/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  redis: {
-    url: '106.13.185.143',
-    port: 6379,
-    password: '@LicoLing123'
-  }
-}
