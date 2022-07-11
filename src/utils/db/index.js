401e9b3a3f8dd05ada826bcb1ab0cd8421dc7b93
凌磊 <320043337@qq.com>
2022-07-11 15:36:29 +0800

    重构路由模块

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -2,8 +2,8 @@ const mysql = require('mysql')
 let mysqlPwd = process.env.NODE_ENV === 'development' ? 'admin123' : '@LicoLing123'
 
 connection = mysql.createConnection({
-  // host: '127.0.0.1',
-  host: '159.138.57.207',
+  host: '127.0.0.1',
+  // host: '159.138.57.207',
   user: 'root',
   password: mysqlPwd,
   // password: '@LicoLing123',
